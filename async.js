6e403e3301f52a5af95bd6238f93a139f35fa11e
Мироненко Юрий <ydtalel@gmail.com>
2023-07-20 15:33:40 +0300

    Update async.js with while loop

--- async.js
+++ async.js
@@ -1,5 +1,5 @@
 const readline = require('readline');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,72 +11,63 @@ const maxAttempts = 5;
 let attempt = 0;
 
 async function guessNum() {
-  console.log(`попытка ${attempt + 1} из ${maxAttempts}\n`);
+  console.log('Угадайте число от 1 до 100:\nУ вас будет 5 попыток.\nВведите "q" что бы сдаться\n');
 
-  try {
-    const guess = await questionAsync('Введи число: ');
+  while (attempt < maxAttempts) {
     attempt += 1;
+    console.log(`попытка ${attempt} из ${maxAttempts}\n`);
+    try {
+      const guess = await questionAsync('Введи число: ');
 
-    switch (true) {
-      case guess === 'q':
-        console.log("Игрок сдался. Игра окончена.\n");
-        rl.close();
-        await appendHistory(`Попытка ${attempt}: Игрок сдался. Игра окончена.\n\n`);
-        return;
-      case isNaN(guess):
-        console.log("Нужно ввести число.");
-        await appendHistory(`Попытка ${attempt}: Нужно ввести число.`);
-        break;
-      case guess > numberToGuess:
-        console.log("Загаданное число меньше");
-        await appendHistory(`Попытка ${attempt}: Загаданное число меньше.`);
-        break;
-      case guess < numberToGuess:
-        console.log("Загаданное число больше");
-        await appendHistory(`Попытка ${attempt}: Загаданное число больше.`);
-        break;
-      case guess == numberToGuess:
-        console.log("Вы угадали!");
-        rl.close();
-        await appendHistory(`Попытка ${attempt}: Игрок выйграл\n\n`);
-        return;
+      switch (true) {
+        case guess === 'q':
+          console.log('Игрок сдался. Игра окончена.\n');
+          rl.close();
+          await appendHistory(`Попытка ${attempt}: Игрок сдался. Игра окончена.\n\n`);
+          return;
+        case isNaN(guess):
+          console.log('Нужно ввести число.');
+          await appendHistory(`Попытка ${attempt}: Нужно ввести число.`);
+          break;
+        case guess > numberToGuess:
+          console.log('Загаданное число меньше');
+          await appendHistory(`Попытка ${attempt}: Загаданное число меньше.`);
+          break;
+        case guess < numberToGuess:
+          console.log('Загаданное число больше');
+          await appendHistory(`Попытка ${attempt}: Загаданное число больше.`);
+          break;
+        case guess == numberToGuess:
+          console.log('Вы угадали!');
+          rl.close();
+          await appendHistory(`Попытка ${attempt}: Игрок выиграл\n\n`);
+          return;
+      }
+    } catch (error) {
+      console.error('Ошибка:', error);
     }
-
-    if (attempt === maxAttempts) {
-      console.log("Закончились попытки");
-      rl.close();
-      await appendHistory("Игрок проиграл. Закончились попытки.\n\n");
-    } else {
-      await guessNum();
-    }
-  } catch (error) {
-    console.error('Ошибка:', error);
   }
+
+  console.log('Закончились попытки');
+  rl.close();
+  await appendHistory('Игрок проиграл. Закончились попытки.\n\n');
 }
 
-function questionAsync(question) {
-  return new Promise((resolve, reject) => {
+async function questionAsync(question) {
+  return new Promise((resolve) => {
     rl.question(question, (answer) => {
       resolve(answer);
     });
   });
 }
 
-function appendHistory(data) {
-  return new Promise((resolve, reject) => {
-    fs.appendFile('history.txt', `${data}\n`, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+async function appendHistory(data) {
+  try {
+    await fs.appendFile('history.txt', `${data}\n`);
+  } catch (error) {
+    console.error('Ошибка при записи в файл:', error);
+    throw error;
+  }
 }
 
-async function main() {
-  console.log('Угадайте число от 1 до 100:\nУ вас будет 5 попыток.\nВведите "q" что бы сдаться\n');
-  await guessNum();
-}
-
-main();
+guessNum();
